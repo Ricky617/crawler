004df906b97a00ddbc1ec6fc19c7097a122e4e57
PUGE <100284685@qq.com>
2018-11-03 17:56:02 +0800

    优化SQL 提升效率

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/md5"
+	"database/sql"
 	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
@@ -13,23 +14,37 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
-// SERVER 母机地址
-const SERVER = "http://127.0.0.1:5000"
+// encrypt 数据是否加密传输
+const encrypt = false
 
-// APISERVER 调用Api接口
-const APISERVER = "https://api.appsign.vip:2688"
+// server 母机地址
+const server = "http://127.0.0.1:8000"
 
+// 调用Api接口
+const apiServer = "https://api.appsign.vip:2688"
+
+// 还没有扫描用户列表
+var unknownUserList = []string{"98448654828"}
+
+// 缓存时间
+var cacheTime int64 = 1041218962781626500
+var cacheToken = ""
+var cacheDevice = ""
+
+// 错误次数
 var errorNumber = 1
+var dbConnect *sql.DB
 
 // 当然是请求任务啦
 func getJob() (string, int) {
-	resp, err := http.Get(SERVER + "/getJob")
+	resp, err := http.Get(server)
 	if err != nil {
 		return err.Error(), 1
 	}
-
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -38,12 +53,11 @@ func getJob() (string, int) {
 	return string(body), 0
 }
 
-func get(requestUrl string) []byte {
+// Get请求数据
+func get(requestURL string) []byte {
 	client := &http.Client{}
-	fmt.Printf(requestUrl)
-	// requestUrl = strings.Replace(requestUrl, "App%20Stroe", "App%2520Stroe", -1)
-	// requestUrl = strings.Replace(requestUrl, "iPhone8,1", "iPhone8%2C1", -1)
-	req, err := http.NewRequest("GET", requestUrl, nil)
+	// fmt.Printf(requestURL)
+	req, err := http.NewRequest("GET", requestURL, nil)
 	if err != nil {
 		// handle error
 	}
@@ -64,6 +78,7 @@ func get(requestUrl string) []byte {
 	return body
 }
 
+// Post请求数据
 func post(url string, data string) []byte {
 	resp, err := http.Post(url,
 		"application/x-www-form-urlencoded",
@@ -82,25 +97,23 @@ func post(url string, data string) []byte {
 
 // 获取新的设备信息:有效期60分钟永久
 func getToken() string {
-	url := APISERVER + "/token/douyin/version/2.7.0"
+	url := apiServer + "/token/douyin/version/2.7.0"
 	res := get(url)
 	type token struct {
 		Success bool
 		Token   string
 	}
 	var tokenData token
-	if err := json.Unmarshal(res, &tokenData); err == nil {
-		log.Println(tokenData.Token)
-		return tokenData.Token
-	} else {
+	if err := json.Unmarshal(res, &tokenData); err != nil {
 		log.Println(err.Error())
+		return ""
 	}
-	return ""
+	return tokenData.Token
 }
 
 // getDevice 请求设备信息
 func getDevice() string {
-	url := APISERVER + "/douyin/device/new/version/2.7.0"
+	url := apiServer + "/douyin/device/new/version/2.7.0"
 	res := get(url)
 	// DEVICE 设备信息
 	type DEVICE struct {
@@ -122,20 +135,19 @@ func getDevice() string {
 		Data    DEVICE
 	}
 	var deviceData device
-	if err := json.Unmarshal(res, &deviceData); err == nil {
-		temp := deviceData.Data
-		return `openudid=` + temp.Openudid + `&idfa=` + temp.Idfa + `&vid=` + temp.Vid + `&install_id=` + strconv.Itoa(temp.Install_id) + `&iid=` + strconv.Itoa(temp.Iid) + `&device_id=` + strconv.Itoa(temp.Device_id) + `&new_user=` + strconv.Itoa(temp.New_user) + `&device_type=` + temp.Device_type + `&os_version=` + temp.Os_version + `&osAPISERVER=` + temp.OsAPISERVER + `&screen_width=` + temp.Screen_width + `&device_platform=` + temp.Device_platform
-	} else {
+	if err := json.Unmarshal(res, &deviceData); err != nil {
 		log.Println(err.Error())
+		return ""
 	}
-	return ""
+	temp := deviceData.Data
+	return `openudid=` + temp.Openudid + `&idfa=` + temp.Idfa + `&vid=` + temp.Vid + `&install_id=` + strconv.Itoa(temp.Install_id) + `&iid=` + strconv.Itoa(temp.Iid) + `&device_id=` + strconv.Itoa(temp.Device_id) + `&new_user=` + strconv.Itoa(temp.New_user) + `&device_type=` + temp.Device_type + `&os_version=` + temp.Os_version + `&osAPISERVER=` + temp.OsAPISERVER + `&screen_width=` + temp.Screen_width + `&device_platform=` + temp.Device_platform
 }
 
 // getSign 获取签名信息
-func getSign(token string, device string) string {
-	url := APISERVER + "/sign"
-	timestamp := strconv.FormatInt(time.Now().UTC().UnixNano(), 10)
-	query := "user_id=98105997680&offset=0&count=49&source_type=2&max_time=" + timestamp[:10] + "&ac=WIFI&" + device + `&version_code=2.7.0&app_version=2.7.0&channel=App%20Stroe&app_name=aweme&build_number=27014&aid=1128`
+func getSign(token string, device string, userID string) string {
+	url := apiServer + "/sign"
+	timestamp := strconv.FormatInt(time.Now().UTC().UnixNano()+3600000000000, 10)
+	query := "user_id=" + userID + "&offset=0&count=49&source_type=2&max_time=" + timestamp[:10] + "&ac=WIFI&" + device + `&version_code=2.7.0&app_version=2.7.0&channel=App%20Stroe&app_name=aweme&build_number=27014&aid=1128`
 	jsonData := `{"token":"` + token + `","query":"` + query + `"}`
 	res := post(url, jsonData)
 	type SIGN struct {
@@ -156,83 +168,135 @@ func getSign(token string, device string) string {
 	return query
 }
 
-// 给请求 params 签名
-func getUserFavorite() {
+// 生成请求参数
+func getQuery(userID string) string {
+	device := cacheDevice
+	token := cacheToken
+	timestamp := time.Now().UTC().UnixNano()
+	// 如果超过了1000秒重新获取设备信息和Token
+	if timestamp > cacheTime+1000000000000 {
+		// 获取个设备信息才好进行下面操作啊
+		device = getDevice()
+		// token当然也是必须的啊
+		token = getToken()
+		// 刷新缓存
+		cacheTime = timestamp
+		cacheDevice = device
+		cacheToken = token
+	} else {
+		log.Println("use cache device and token")
+	}
+	// 生成访问参数
+	query := getSign(token, device, userID)
+	return url.PathEscape(query)
+}
+
+func getUserFavoriteList(userID string) []map[string]string {
+	userDataList := make([]map[string]string, 0)
 	// 这个一看就知道是抖音官方接口啊
 	getURL := "https://aweme.snssdk.com/aweme/v1/user/following/list/?"
-	// 获取个设备信息才好进行下面操作啊
-	device := getDevice()
-	// token当然也是必须的啊
-	token := getToken()
-	// 生成抖音分辨不出来是外人的访问参数
-	query := getSign(token, device)
-	res := get(getURL + url.PathEscape(query))
-	log.Println(string(res))
+	query := getQuery(userID)
+	res := get(getURL + query)
+	// log.Println(string(res))
 	// 解析返回的有什么东东
 	var follow interface{}
 	if err := json.Unmarshal(res, &follow); err == nil {
 		// 我猜他应该是这个格式数据
 		resData := follow.(map[string]interface{})
-		// 其他脚本语言解析JSON不知道比GO语言方便到哪里去了(我技术渣)
 		statusCode := resData["status_code"].(float64)
 		if int(statusCode) == 0 {
-			log.Println("获取关注列表成功!")
+			// 获取完当前用户的关注信息后将当前用户移出用户池
+			unknownUserList = unknownUserList[1:]
+			// 清洗数据
 			followings := resData["followings"].([]interface{})
-			userDataList := make([]map[string]string, 0)
+			// 计算数据库插入时间
+			t1 := time.Now()
 			for _, workItem := range followings {
 				followItem := workItem.(map[string]interface{})
-				userData := map[string]string{
-					"signature": followItem["signature"].(string),
-					"nickname":  followItem["nickname"].(string),
-					"uid":       followItem["uid"].(string),
+				// 如果在数据库中已经存在的则不进行保存
+				if !checkUserSaved(followItem["uid"].(string)) {
+					userData := map[string]string{
+						"signature": followItem["signature"].(string),
+						"nickname":  followItem["nickname"].(string),
+						"uid":       followItem["uid"].(string),
+						"birthday":  followItem["birthday"].(string),
+					}
+					userDataList = append(userDataList, userData)
+					// 限制用户池大小防止溢出 上限200
+					if len(unknownUserList) < 200 {
+						// 添加到还没有扫描列表
+						unknownUserList = append(unknownUserList, followItem["uid"].(string))
+					}
 				}
-				userDataList = append(userDataList, userData)
-
-				// go的转JSON太难用
-				// log.Println(followItem)
 			}
-			jsonUserData, _ := json.Marshal(userDataList)
-			// 进行MD5加密
-			h := md5.New()
-			h.Write(jsonUserData)
-			md5 := hex.EncodeToString(h.Sum(nil))
-			// 解析完了就把解析成功的数据发给母机
-			post(SERVER+"/return", `{code:0,data:`+md5+base64.StdEncoding.EncodeToString(jsonUserData)+`}`)
+			time := time.Now().Sub(t1)
+			// 输出当前用户池
+			log.Println(time)
+			log.Println("------------------------- users pool -------------------------")
+			log.Println(unknownUserList)
+			log.Println("--------------------------------------------------------------")
 		}
 	} else {
 		log.Println(err.Error())
 	}
+	return userDataList
 }
 
-func work(workList string) {
-	md5Data := workList[:32]
-	data := workList[32:]
-	// fmt.Print(data)
-	decodedData, err := base64.StdEncoding.DecodeString(data)
-	// 解析失败了那0.01%是伪造的请求, 99.9%是服务端挂了,那还执行毛任务啊歇着吧
-	if err == nil {
-		has := md5.Sum(decodedData)
-		md5Check := fmt.Sprintf("%x", has)
-		if md5Data == md5Check {
-			// 开始解析服务器发来什么玩意
-			var message interface{}
-			if err := json.Unmarshal(decodedData, &message); err == nil {
-				resData := message.(map[string]interface{})
-				// fmt.Println(reflect.TypeOf(resData["data"]))
-				workList := resData["data"].([]interface{})
-				for _, workItem := range workList {
-					fmt.Println(workItem)
-				}
-				getUserFavorite()
+func work(workList string, userID string) {
+	decodedData := []byte(workList)
+	// 如果设置了加密需要先解密
+	if encrypt {
+		md5Data := workList[:32]
+		data := workList[32:]
+		// fmt.Print(data)
+		decodedData, err := base64.StdEncoding.DecodeString(data)
+		// 解析失败了那0.01%是伪造的请求, 99.9%是服务端挂了,那还执行毛任务啊歇着吧
+		if err == nil {
+			has := md5.Sum(decodedData)
+			md5Check := fmt.Sprintf("%x", has)
+			if md5Data == md5Check {
+				fmt.Printf("数据MD5校验失败!")
+				return
 			}
+		} else {
+			fmt.Printf(err.Error())
+			return
 		}
-	} else {
-		fmt.Printf(err.Error())
+	}
+	// 开始解析服务器发来什么玩意
+	var message interface{}
+	if err := json.Unmarshal(decodedData, &message); err == nil {
+		resData := message.(map[string]interface{})
+		// fmt.Println(reflect.TypeOf(resData["data"]))
+		workList := resData["data"].([]interface{})
+		for _, workItem := range workList {
+			fmt.Println(workItem)
+		}
 	}
 }
 
+// 获取抖音指定用户关注列表
+func getDouYinFavorite(userID string) {
+	// 获取到关注用户列表
+	userFavoriteList := getUserFavoriteList(userID)
+	favoriteList, _ := json.Marshal(userFavoriteList)
+	sendData := string(favoriteList)
+	if encrypt {
+		// fmt.Print(string(sendData))
+		// 进行MD5加密
+		h := md5.New()
+		h.Write(favoriteList)
+		md5Data := hex.EncodeToString(h.Sum(nil))
+		sendData = md5Data + base64.StdEncoding.EncodeToString(favoriteList)
+	}
+	log.Println("save user number:" + strconv.Itoa(len(userFavoriteList)))
+	// print(`{"err":0,"data":` + sendData + `}`)
+	// 解析完了就把解析成功的数据发给母机
+	post(server+"/return", `{"err":0,"data":`+sendData+`}`)
+}
+
 // 获取工作的函数
-func getWork() {
+func getWork(userID string) {
 	// 没有找到工作要你有毛用啊 继续找
 	data, err := getJob()
 	if err != 0 {
@@ -243,7 +307,7 @@ func getWork() {
 				fmt.Println("第" + strconv.Itoa(errorNumber) + "次尝试与服务器建立连接失败,1分钟后重试!")
 				errorNumber++
 			} else {
-				work(data)
+				work(data, userID)
 			}
 			// 请求100次服务器都返回错误 100%是我的垃圾服务挂了 洗洗睡吧
 			if errorNumber > 10 {
@@ -254,15 +318,36 @@ func getWork() {
 			// 没有找到工作要你有毛用啊 继续找
 		}
 	} else {
-		work(data)
+		work(data, userID)
 	}
 }
 
+func checkUserSaved(douyinID string) bool {
+	var count int
+	err := dbConnect.QueryRow("select isnull((select top(1) 1 from DouYin.dbo.SIMPLE where DOUYIN_ID = '" + douyinID + "'), 0)").Scan(&count)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if count > 0 {
+		return true
+	}
+	return false
+}
+
 func main() {
+	// 连接数据库
+	conn, err := sql.Open("mssql", "server=localhost;user id=PUGE;password=mmit7750;")
+	if err != nil {
+		log.Fatal("Open connection failed:", err.Error())
+	}
+	dbConnect = conn
 	// 请求100次服务器都返回错误 100%是我的垃圾服务挂了 洗洗睡吧
 	if errorNumber > 100 {
 		// 问我为什么是中文输出(这样岂不是很没有逼格?), 我的回答是:要照顾每个使用者的智商(我其实也不懂英文啊!!!)
 		fmt.Println("与服务器建立连接失败!")
 	}
-	getWork()
+	// 干不完不准休息
+	for len(unknownUserList) > 0 {
+		getDouYinFavorite(unknownUserList[0])
+	}
 }
--- server/main.py
+++ server/main.py
@@ -1,30 +1,73 @@
+#!/usr/bin/env python
+#--coding:utf-8--
+import urllib
 import json
-import hashlib
-import base64
-from flask import Flask, request
-app = Flask(__name__)
+import time
+import pyodbc
+import logging
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-# md5 = hashlib.md5(b'123').hexdigest()
-# print(md5)
-
-# 加密
-def encryption (original):
-  md5Data = hashlib.md5(original).hexdigest()
-  base64Data = base64.b64encode(original)
-  return md5Data + str(base64Data, encoding = "utf8")
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(filename)s:%(lineno)d %(threadName)s:%(funcName)s %(levelname)s] %(message)s')
 
 jobList = ['1212', '121111']
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+  # GET
+  def do_GET(self):
+    sendData = {"err": 0, "data": jobList}
+    self.outputtxt(json.dumps(sendData))
 
-@app.route('/getJob')
-def index():
-  sendData = {"err": 0, "data": jobList}
-  return encryption(json.dumps(sendData).encode())
+  def do_POST(self):
+    content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+    post_data = self.rfile.read(content_length) # <--- Gets the data itself
+    logging.debug("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n", str(self.path), str(self.headers), post_data.decode('utf-8'))
+    # print(resData)
+    # 接收到的数据
+    resData = json.loads(post_data.decode('utf-8'))
+    # 解析出用户列表数据
+    userList = resData["data"]
+    if (resData["err"] == 0 and len(userList) > 0):
+      # 连接数据库
+      conn = pyodbc.connect(r'DRIVER={SQL Server Native Client 11.0};SERVER=localhost;DATABASE=Douyin;UID=PUGE;PWD=mmit7750')
+      # 获取数据库指针
+      c = conn.cursor()
+      logging.info('receive ' + str(len(userList)) +' user info!')
+      logging.debug(userList)
+      # 拼接SQL语句一次性插入
+      sqlStr = 'INSERT INTO SIMPLE (DOUYIN_ID, NAME, SIGNA, BIRTHDAY, GET_TIME) VALUES '
+      # 获取当前时间戳
+      Time = time.time()
+      for ind, val in enumerate(userList):
+        # 去除非法字符
+        val['nickname'] = val['nickname'].replace('\n', '')
+        val['nickname'] = val['nickname'].replace("'", "''")
+        val['signature'] = val['signature'].replace('\n', '')
+        val['signature'] = val['signature'].replace("'", "''")
+        if (len(userList) - 1 == ind):
+          sqlStr += "(" + val['uid'] +", '" + val['nickname'] +"', '" + val['signature'] +"', '" + val['birthday'] +"', " + str(int(Time)) +" );"
+        else:
+          sqlStr += "(" + val['uid'] +", '" + val['nickname'] +"', '" + val['signature'] +"', '" + val['birthday'] +"', " + str(int(Time)) +" ),"
+      # print(sqlStr)
+      # 插入数据库
+      c.execute(sqlStr)
+      conn.commit()
+      conn.close()
+    self.outputtxt(json.dumps({"err": 0}))
+ 
+  def outputtxt(self, content):
+    self.send_response(200)
+    self.send_header('Content-type', 'application/json')
+    self.end_headers()
+    self.wfile.write(bytes(content, "utf-8"))
 
-@app.route('/return', methods=['POST'])
-def returnData():
-  print(request.form)
-  print(request.form.get('data','default value'))
-  return 'welcome'
+def run():
+  port = 8000
+  print('starting server, port', port)
+
+  # Server settings
+  server_address = ('0.0.0.0', port)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('running server...')
+  httpd.serve_forever()
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  run()
